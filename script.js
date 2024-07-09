ea28d832e3ad17d3471488973eeedf9039e93174
navidvfie <info@navidvafaei.ir>
2024-07-09 17:50:42 +0330

    v1 (complete base project)

--- script.js
+++ script.js
@@ -8,7 +8,23 @@ function submitForm(){
     const mortgageTermValue = Number(mortgageTerm.value) * 12 ;
     const interestRateValue =Number(interestRate.value)/ 100 / 12;
     const moneyRepay = mortgageAmountValue * interestRateValue * (Math.pow(1 + interestRateValue, mortgageTermValue)) / (Math.pow(1 + interestRateValue, mortgageTermValue) - 1);
-    console.log(moneyRepay.toFixed(2))
-    typeof(moneyRepay)
+    document.querySelector(".right-side").innerHTML=`      <h2>Your results</h2>
+      <p>  Your results are shown below based on the information you provided. 
+        To adjust the results, edit the form and click “calculate repayments” again.
+      </p>
+      <div id="right-side-result">
+        <div id="monthly-repayments">
+          <p>Your monthly repayments</p>
+          <span class="monthly-repayments">243242</span>
+        </div>
+        <div id="total-repay">
+          <p>Total you'll repay over the term </p>
+          <span class="total-repay">4234234322343</span>
+        </div>
+      </div>`;
+      const moneyRepayFloor = moneyRepay.toFixed(2);
+    document.querySelector(".monthly-repayments").textContent =moneyRepayFloor;
+    const totalRepay =(moneyRepayFloor * mortgageTermValue).toFixed(2);
+    document.querySelector(".total-repay").textContent =totalRepay;
 }
 
